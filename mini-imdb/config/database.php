a8863954fa55686db53af649f7fb536589dfdc08
Hosseinreza <hrakbari77@gmail.com>
2024-01-26 14:09:09 +0330

    Add elastic search config

--- mini-imdb/config/database.php
+++ mini-imdb/config/database.php
@@ -95,6 +95,20 @@ return [
 
     ],
 
+
+    'elasticsearch' => [
+        'driver' => 'elasticsearch',
+        'hosts' => [
+            [
+                'host' => env('ELASTICSEARCH_HOST', 'localhost'),
+                'port' => env('ELASTICSEARCH_PORT', 9200),
+                'scheme' => env('ELASTICSEARCH_SCHEME', 'http'),
+                'user' => env('ELASTICSEARCH_USER'),
+                'pass' => env('ELASTICSEARCH_PASS'),
+            ],
+        ],
+    ],
+
     /*
     |--------------------------------------------------------------------------
     | Migration Repository Table
